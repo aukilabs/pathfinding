8fc9a7b1c2525ad0b3dbc7559f1966d09c2796e5
Jason Yankowski <jason@aukilabs.com>
2025-11-19 13:20:36 +0800

    re-export navgraph types for convenience

--- auki-pathfinding-js/src/index.ts
+++ auki-pathfinding-js/src/index.ts
@@ -6,3 +6,6 @@ export * from "./navgraph/PathfindingUtils";
 export * from "./navgraph/RecastUtils";
 export * from "./navgraph/Pathfinder";
 export * from "./navgraph/FloorData";
+
+//re-export navgraph code
+export * from "@auki/navgraph";
